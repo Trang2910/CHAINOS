2200e9e9a4f56dc1c432e7e59a45f868fde8d20c
Trang2910 <trangnt72@wru.vn>
2019-12-09 20:46:03 +0700

    the first time

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="css/index.css">
+	<!-- <link rel="stylesheet" href="bootstrap-4.3.1-dist/css/bootstrap.min.css"> -->
+	<!-- <script src="js/jquery-3.4.1.min.js"></script> -->
+	<!-- <script src="bootstrap-4.3.1-dist/js/bootstrap.min.js"></script> -->
+	<title>Demo</title>
+</head>
+<body>
+	<div class="header">
+		<div class="logo">	
+			<img src="img/logo.PNG" alt="logo">
+		</div>
+		<div class="title">
+			<ul>
+				<li><a href="">HTML TEMPLATES</a></li>
+				<li><a href="">HTML TEMPLATES 2</a></li>
+				<li><a href="">WATCH VIDEO</a></li>
+				<li><input type="submit" value="DOWNLOAD NOW"></li>
+				<li></li>
+			</ul>
+		</div>
+	</div>
+	<div class="main">
+		<div class="item-1">
+			<div class="head">
+				<h1>About us</h1>
+			</div>
+			<div class="content">
+				<div class="img">
+					<img src="img/computer.PNG" alt="computer">
+				</div>
+				<div class="side">
+					<small>Welcome! You've Found the Easiest Way to Learn HTML and CSS</small>
+					<h4>Simple HTML Website Template</h4>
+					<h5>Give you listeners a professional record of your work!</h5>
+					<p>
+						-Professionally we will write a song;<br>
+						-We will help to make an advertising audio-movie or jingles;<br>
+						-We will voice the video;<br>
+						-We will arrange for the author's song;
+					</p>
+				</div>
+			</div>
+		</div>
+		<div class="item-2">
+			<div class="head">
+				<h1>About the studio</h1>
+			</div>
+			<div class="content"></div>
+		</div>
+	</div>
+	<div class="footer">
+		
+	</div>
+</body>
+</html>
