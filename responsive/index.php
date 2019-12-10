63ef6c0c947f3822faf6b08af6f7f78754e49dc6
Trang2910 <trangnt72@wru.vn>
2019-12-10 23:53:51 +0700

    res

---
+++ responsive/index.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="css/index.css">
+	<link rel="stylesheet" href="bootstrap-4.3.1-dist/css/bootstrap.min.css">
+	<script src="js/jquery-3.4.1.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+	<script src="bootstrap-4.3.1-dist/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+	<title>Báo mới-Tin tức nóng nhất</title>
+</head>
+<body>
+	<div class="header">
+			<div class="logo">
+				<img src="http://leadmedia.vn/data/upload/baomoi.comlogo.png" class="img-fluid" alt="logo">
+			</div>
+			<div class="search">
+				<form style="padding: 6px 12px;">
+					<input type="text" placeholder="Nhập nội dung tìm kiếm">
+					<i class="fas fa-search"></i>
+				</form>
+			</div>
+			<div class="icon">
+				<div class="dropdown">
+				    <button class="btn" type="button" data-toggle="dropdown">
+				    	<i class="fas fa-user"></i>
+				    </button>
+				    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+				  		<span><b>Đăng nhập</b></span><br>
+						<sub><i>Đăng nhập để trải nghiệm thêm những tính năng hữu ích</i></sub>
+				    	<a class="dropdown-item" href="#"><i class="fab fa-twitter"></i> Twitter</a>
+				    </div>
+				</div>
+				<div style="padding: 6px 12px;"><a href=""><i class="fas fa-bell" style="color: black;"></i></a></div>
+			</div>
+	</div>
+	<div style="height: 4em">
+		<nav class="navbar navbar-expand-lg navbar-light">
+  		<button class="navbar-toggler" type="button" data-toggle="dropdown">
+    		<span class="navbar-toggler-icon"></span>
+  		</button>
+	  	<div class="dropdown-menu collapse navbar-collapse" id="navbarNav">
+		    <ul class="navbar-nav">
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">XÃ HỘI</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">THẾ GIỚI</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">VĂN HÓA</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">KINH TẾ</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">GIÁO DỤC</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">THỂ THAO</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">GIẢI TRÍ</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">PHÁP LUẬT</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">CÔNG NGHỆ</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">KHOA HỌC</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">ĐỜI SỐNG</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">XE CỘ</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">NHÀ ĐẤT</a>
+		      </li>
+		    </ul>
+		</div>
+	</nav>
+	</div>
+	
+	<div class="main">
+		<div class="left">
+				<a href=""><i class="fas fa-fire-alt"></i><span> NÓNG</span></a>
+				<a href=""><i class="far fa-clock"></i><span> MỚI</span></a>
+				<a href=""><i class="fas fa-video"></i><span> VIDEO</span></a>
+				<a href=""><i class="fas fa-layer-group"></i><span> CHỦ ĐỀ</span></a>
+				<a href=""><i class="fas fa-award"></i><span> ĐỀ XUẤT</span></a>
+		</div>
+		<div class="mid">
+			<a href=""><img src="https://photo-3-baomoi.zadn.vn/w500_r16x9_sm/2019_12_10_119_33269114/462e46debf9e56c00f8f.jpg.webp" alt=""></a>
+			<a href=""><h3>U22 Việt Nam lần đầu giành HCV bóng đá nam SEA Games</h3></a>
+		</div>
+		<div class="right">
+				<ul>
+					<li>
+						<a href=""><img src="https://baomoi-static.zadn.vn/events/event_NPT_ava_04-min.png" alt=""></a>
+						<a href=""><h5>Tổng Bí thư, Chủ tịch nước Nguyễn Phú Trọng</h5></a>
+					</li>
+					<li>
+						<a href=""><img src="https://photo-3-baomoi.zadn.vn/w100_r1x1/2019_12_10_240_33268319/88b6c9b531f5d8ab81e4.jpg" alt=""></a>
+						<a href=""><h5>Idol Kpop nào được quan tâm nhiều nhất trên mạng xã hội Twitter 2019?</h5></a>
+					</li>
+					<li>
+						<a href=""><img src="https://photo-1-baomoi.zadn.vn/w100_r1x1/2019_12_10_240_33268320/f766b4654c25a57bfc34.jpg" alt=""></a>
+						<a href=""><h5>Mã đề thi toàn tên cầu thủ, vừa hài vừa 'thách đố' học sinh quay bài</h5></a>
+					</li>
+					<li>
+						<a href=""><img src="https://photo-atm-baomoi.zadn.vn/w200_r1x1/adtima-media.zadn.vn/2019/12/77bfec1b-6a82-4eca-ad69-02eb0d791883.png" alt=""></a>
+						<a href=""><h5>Độc quyền phân phối: Ra mắt Robot hút bụi Medion MD18500 phiên bản Limited</h5></a>
+					</li>
+				</ul>
+		</div>
+	</div>
+	<div class="footer"></div>
+</body>
+</html>
